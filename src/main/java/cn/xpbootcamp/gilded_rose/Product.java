edccbbf8420ee0cc2d3639bfbbc14f6c4018e935
hongyu.li <hongyu.li@thoughtworks.com>
2020-03-24 02:17:03 +0800

    [Hongyu]refactor: organize quality and sell in update policy

--- src/main/java/cn/xpbootcamp/gilded_rose/Product.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Product.java
@@ -40,28 +40,14 @@ public class Product {
     }
 
     public void updateSellIn(int update_period) {
-        if (this.sellIn + update_period < 0) {
-            this.sellIn = 0;
-        }else{
-            this.sellIn += update_period;
-        }
+        this.sellIn = Math.max(0, this.sellIn + update_period);
     }
 
     public void updateQuality(int quality_variation) {
         if (this.sellIn != 0) {
-            if (this.quality + quality_variation < 0) {
-                this.quality = 0;
-            }else if (this.quality + quality_variation > 50) {
-                this.quality = 50;
-            } else{
-                this.quality += quality_variation;
-            }
+            this.quality = Math.min(Math.max(0, this.quality + quality_variation), 50);
         }else{
-            if (this.quality + quality_variation * 2 < 0) {
-                this.quality = 0;
-            }else{
-                this.quality += quality_variation * 2;
-            }
+            this.quality = Math.max(0, this.quality + quality_variation * 2);
         }
     }
 }
